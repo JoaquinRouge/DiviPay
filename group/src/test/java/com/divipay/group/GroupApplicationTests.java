ce8b28687fba0632663cc328910f3e399f6d0f41
JoaquinRouge <Joarouge@gmail.com>
2025-06-15 20:13:02 -0300

    Initialized empty services

---
+++ group/src/test/java/com/divipay/group/GroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divipay.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
