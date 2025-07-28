44d98638fc490cad01817ed8a5e0e7dd8e6f706b
Joaquin <joarouge@gmail.com>
2025-07-28 02:32:33 -0300

    Friend service added to project

---
+++ friends/src/test/java/com/divipay/friends/friends/FriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divipay.friends.friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
